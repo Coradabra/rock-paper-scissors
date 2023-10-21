b7d5ff8739ac6611903eec3c1064e673fe9a2c7a
Cory Bradbury <coradabra@gmail.com>
2023-10-22 10:54:08 +1100

    Fix game logic and add additional UI components
    Fix click listener callback function so function isn't had at initialization
    
    Fix random logic for computer choice so it stops always returning SCISSORS
    
    Add image assests folder and avatar for player and computer score
    
    Fix style.css so footer is always at the bottom of the page
    
    Add additional css styling to improve appearance

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
 
-rockBtn.addEventListener("click", playRound("ROCK"));
-paperBtn.addEventListener("click", playRound("PAPER"));
-scissorsBtn.addEventListener("click", playRound("SCISSORS"));
-
+const outcomeText = document.querySelector(".outcome-text");
 const playerScoreDisplay = document.querySelector(".player-score");
 const computerScoreDisplay = document.querySelector(".computer-score");
 
-function getComputerChoice() {
-  const randNum = Math.random() * 3;
+rockBtn.addEventListener("click", () => playRound("ROCK"));
+paperBtn.addEventListener("click", () => playRound("PAPER"));
+scissorsBtn.addEventListener("click", () => playRound("SCISSORS"));
 
-  if (randNum === 1) {
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const randNum = Math.random();
+
+  if (randNum > 0.33) {
     return "ROCK";
-  } else if (randNum === 2) {
+  } else if (randNum < 0.66) {
     return "PAPER";
   } else {
     return "SCISSORS";
@@ -23,38 +27,41 @@ function getComputerChoice() {
 
 function playRound(playerSelection) {
   const computerSelection = getComputerChoice();
-  let playerScore = 0;
 
   if (playerSelection === computerSelection) {
-    console.log(`Draw! You both selected ${playerSelection}`);
+    outcomeText.textContent = `Draw! You both selected ${playerSelection}`;
   } else {
     if (playerSelection === "ROCK") {
       if (computerSelection === "PAPER") {
-        console.log("You lose! Paper beats rock.");
+        outcomeText.textContent = "You lose! Paper beats rock.";
+        computerScore++;
       } else {
+        outcomeText.textContent = "You win! Rock beats scissors";
         playerScore++;
-        console.log("You win! Rock beats scissors");
       }
     }
 
     if (playerSelection === "PAPER") {
       if (computerSelection === "SCISSORS") {
-        console.log("You lose! Scissors beats paper.");
+        outcomeText.textContent = "You lose! Scissors beats paper.";
+        computerScore++;
       } else {
+        outcomeText.textContent = "You win! Paper beats rock";
         playerScore++;
-        console.log("You win! Paper beats rock");
       }
     }
 
     if (playerSelection === "SCISSORS") {
       if (computerSelection === "ROCK") {
-        console.log("You lose! Rock beats scissors.");
+        outcomeText.textContent = "You lose! Rock beats scissors.";
+        computerScore++;
       } else {
+        outcomeText.textContent = "You win! Scissors beats paper";
         playerScore++;
-        console.log("You win! Scissors beats paper");
       }
     }
   }
 
-  return playerScore;
+  playerScoreDisplay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
 }
