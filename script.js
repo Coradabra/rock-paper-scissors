a2f0aac99a9defe33eea80e6bc4cfbd9a9285c67
Cory Bradbury <coradabra@gmail.com>
2023-10-22 11:31:46 +1100

    Complete game logic
    Add function to check if someone has won and reset the game when complete
    
    Add a finish message for who has won overall and working play again button

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ const outcomeText = document.querySelector(".outcome-text");
 const playerScoreDisplay = document.querySelector(".player-score");
 const computerScoreDisplay = document.querySelector(".computer-score");
 
+const playAgainBtn = document.querySelector(".conclusion button");
+const conclusionText = document.querySelector(".conclusion p");
+
 rockBtn.addEventListener("click", () => playRound("ROCK"));
 paperBtn.addEventListener("click", () => playRound("PAPER"));
 scissorsBtn.addEventListener("click", () => playRound("SCISSORS"));
+playAgainBtn.addEventListener("click", () => resetGame());
 
 let playerScore = 0;
 let computerScore = 0;
+let playing = true;
+let message = "";
 
 function getComputerChoice() {
   const randNum = Math.random();
@@ -28,40 +34,73 @@ function getComputerChoice() {
 function playRound(playerSelection) {
   const computerSelection = getComputerChoice();
 
-  if (playerSelection === computerSelection) {
-    outcomeText.textContent = `Draw! You both selected ${playerSelection}`;
-  } else {
-    if (playerSelection === "ROCK") {
-      if (computerSelection === "PAPER") {
-        outcomeText.textContent = "You lose! Paper beats rock.";
-        computerScore++;
-      } else {
-        outcomeText.textContent = "You win! Rock beats scissors";
-        playerScore++;
+  if (playing) {
+    if (playerSelection === computerSelection) {
+      message = `Draw! You both selected ${playerSelection.toLowerCase()}.`;
+    } else {
+      if (playerSelection === "ROCK") {
+        if (computerSelection === "PAPER") {
+          message = "You lose! Paper beats rock.";
+          computerScore++;
+        } else {
+          message = "You win! Rock beats scissors";
+          playerScore++;
+        }
       }
-    }
 
-    if (playerSelection === "PAPER") {
-      if (computerSelection === "SCISSORS") {
-        outcomeText.textContent = "You lose! Scissors beats paper.";
-        computerScore++;
-      } else {
-        outcomeText.textContent = "You win! Paper beats rock";
-        playerScore++;
+      if (playerSelection === "PAPER") {
+        if (computerSelection === "SCISSORS") {
+          message = "You lose! Scissors beats paper.";
+          computerScore++;
+        } else {
+          message = "You win! Paper beats rock";
+          playerScore++;
+        }
       }
-    }
 
-    if (playerSelection === "SCISSORS") {
-      if (computerSelection === "ROCK") {
-        outcomeText.textContent = "You lose! Rock beats scissors.";
-        computerScore++;
-      } else {
-        outcomeText.textContent = "You win! Scissors beats paper";
-        playerScore++;
+      if (playerSelection === "SCISSORS") {
+        if (computerSelection === "ROCK") {
+          message = "You lose! Rock beats scissors.";
+          computerScore++;
+        } else {
+          message = "You win! Scissors beats paper";
+          playerScore++;
+        }
       }
     }
   }
 
+  updateUi(playerScore, computerScore, message);
+  checkWinner();
+}
+
+function updateUi(playerScore, computerScore, message) {
   playerScoreDisplay.textContent = playerScore;
   computerScoreDisplay.textContent = computerScore;
+  outcomeText.textContent = message;
+}
+
+function checkWinner() {
+  if (playerScore >= 5) {
+    playing = false;
+    conclusionText.textContent = "Condragulations! You win!";
+    playAgainBtn.hidden = false;
+  }
+  if (computerScore >= 5) {
+    playing = false;
+    conclusionText.textContent = "You lose! Better luck next time.";
+    playAgainBtn.hidden = false;
+  }
+  return;
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playing = true;
+
+  conclusionText.textContent = "";
+  playAgainBtn.hidden = true;
+
+  updateUi(0, 0, "Make your choice");
 }
